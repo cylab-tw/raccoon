72a1f515738883caf2388a28dc657ff1d6dd1df7
chin <a5566qq2581@gmail.com>
2021-03-13 22:12:33 +0800

    feat: Added DICOMWeb response
    400 , 404 ,500 Error http response function

---
+++ models/DICOMWeb/httpMessage.js
@@ -0,0 +1,44 @@
+function sendServerWrongMessage (res , value) {
+    let message = {
+        "Details" : value, 
+        "HttpStatus" : 500,
+        "Message" : "Server Wrong",
+        "Method" : "GET",
+    }
+    res.status(500).send(message);
+    res.end();
+}
+
+function sendBadRequestMessage (res , value) {
+    let message = {
+        "Details" : value, 
+        "HttpStatus" : 400,
+        "Message" : "Bad request",
+        "Method" : "GET",
+    }
+    res.status(400).send(message);
+    res.end();
+}
+
+function sendNotFoundMessage (req , res){
+    let message = {
+        "Details" : "Accessing an inexistent Item\r\n", 
+        "HttpStatus" : 404,
+        "Message" : "Not Found",
+        "Method" : "GET",
+    }
+    let notFoundStr = []
+    for (let i in req.query) {
+        notFoundStr.push(`${i}:${req.query[i]}`);
+    }
+    message.Details += notFoundStr.join(',\r\n');
+    res.status(404).send(message);
+    res.end(); 
+}
+
+module.exports =  {
+    sendBadRequestMessage : sendBadRequestMessage , 
+    sendNotFoundMessage : sendNotFoundMessage ,
+    sendServerWrongMessage : sendServerWrongMessage
+}
+

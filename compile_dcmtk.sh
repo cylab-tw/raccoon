40027ebd78e224f174b30321919238e77b7c6ae6
chin <a5566qq2581@gmail.com>
2021-03-30 20:37:25 +0800

    build: Add shell script to build dcmtk
    chore: Add build item ignore

---
+++ compile_dcmtk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+tempPWD=`pwd`
+git clone https://github.com/Chinlinlee/dcmtk.git
+mkdir dcmtk-build
+cd dcmtk-build
+cmake -DDCMTK_MODULES:STRING="ofstd;oflog;dcmdata;" \
+-DBUILD_SINGLE_SHARED_LIBRARY:BOOL=1 \
+-DCMAKE_CXX_FLAGS:STRING="-fPIC" \
+-DCMAKE_C_FLAGS:STRING="-fPIC" ../dcmtk
+
+make -j8
+make DESTDIR=$tempPWD/models/dcmtk/linux-lib install
+rm -rf $tempPWD/dcmtk-build
+rm -rf $tempPWD/dcmtk

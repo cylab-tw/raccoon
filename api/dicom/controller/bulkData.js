71e08d690bd8666426af3e6882bc0ca6d18c74b4
a5566qq123 <a5566qq2581@gmail.com>
2021-11-03 09:30:44 +0800

    fix: missing header `content-type` when retrieve bulkdata

--- api/dicom/controller/bulkData.js
+++ api/dicom/controller/bulkData.js
@@ -3,7 +3,12 @@ const _ = require('lodash');
 const uuid = require('uuid');
 const fs = require('fs');
 
-
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @returns 
+ */
 module.exports = async function (req , res) {
     let key = req.params.objKey;
     let instanceUID = req.params.instanceUID;
@@ -20,6 +25,7 @@ module.exports = async function (req , res) {
     let bulkData = fs.readFileSync(`${process.env.DICOM_STORE_ROOTPATH}${metadata._doc.filename}`);
 
     const BOUNDARY = `${uuid.v4()}-${uuid.v4()}`;
+    res.set("Content-Type" , `multipart/related; type=application/octet-stream; boundary=${BOUNDARY}`);
     res.write(`--${BOUNDARY}\r\n`);
     res.write(`Content-Type:multipart/related; type=application/octet-stream; boundary=${BOUNDARY}\r\n`);
     res.write('Content-length: ' + bulkData.length + '\r\n\r\n');

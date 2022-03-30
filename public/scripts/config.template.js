aecc7ad7e5c0f578667218150fc7822c7eed290b
chin <a5566qq2581@gmail.com>
2022-03-30 11:51:23 +0800

    build: add config template instead `config.js`

---
+++ public/scripts/config.template.js
@@ -0,0 +1,20 @@
+var envConfig = {
+    QIDO: {
+        hostName: '127.0.0.1',
+        port: '9090',
+        api: 'dicom-web',
+        http: "http"
+    },
+    WADO: {
+        hostName: '127.0.0.1',
+        port: '9090',
+        api: 'dicom-web',
+        http: "http"
+    },
+    FHIR: {
+        hostName: '127.0.0.1',
+        port: '9090',
+        api: 'api/fhir',
+        http: "http"
+    }
+}

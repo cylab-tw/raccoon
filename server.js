7a13042e452efd80ae4731f4e781d351d61a82b0
chin <a5566qq2581@gmail.com>
2022-06-04 01:17:16 +0800

    fix: not allow apikey, user-agent headers
    - Add apikey, user-agent headers to support CORS

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(passport.session());
 //app.use(expressValidator());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept ,Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept ,Authorization, apikey, user-agent");
   res.header("Vary", "Origin");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials", "true");

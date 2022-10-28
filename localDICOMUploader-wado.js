10f5c31353ccd2c377803e08ca6416fb63a8a8f9
chin <a5566qq2581@gmail.com>
2022-10-28 20:13:10 +0800

    feat: add tool for uploading DICOM via DICOMweb
    - Change STOW_URL to your raccoon URL in localDICOMUploader-wado.js

---
+++ localDICOMUploader-wado.js
@@ -0,0 +1,69 @@
+require("rootpath")();
+require("dotenv").config();
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+const os = require("os");
+const request = require('request-compose').extend({
+    Request: {
+        multipart: require('request-multipart')
+    }
+}).client;
+
+let osPlatform = os.platform().toLocaleLowerCase();
+if (osPlatform.includes("linux")) {
+    process.env.ENV = "linux";
+} else if (osPlatform.includes("win")) {
+    process.env.ENV = "windows";
+}
+
+let filePath = process.argv[2];
+const STOW_URL = "http://localhost:8081/dicom-web/studies";
+
+async function storeInstance(filename, stowUrl) {
+    let stream = fs.createReadStream(filename);
+    
+    let response = await request({
+        method: "POST",
+        url: stowUrl,
+        headers: {
+            "Content-Type": "multipart/related; type=application/dicom"
+        },
+        multipart: [
+            {
+                "Content-Type": "application/dicom",
+                "Content-Disposition": `attachment; filename="${filename}"`,
+                body: stream
+            }
+        ],
+        timeout: 300000
+    });
+    return response;
+}
+async function main() {
+    console.log(filePath);
+    let successFiles = [];
+    glob("**/*.dcm", { cwd: filePath }, async function (err, matches) {
+        for (let file of matches) {
+            let fullFilename = path.join(filePath, file);
+            try {
+                let response = await storeInstance(fullFilename, STOW_URL);
+                let statusCode = response.res.statusCode;
+                if (statusCode === 200) {
+                    console.log("success: " + fullFilename);
+                } else {
+                    console.error("error: " + response.body.result);
+                    fs.appendFile("upload-error.txt", `error file: ${fullFilename}\r\n`);
+                }
+                
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+}
+(async () => {
+    await main();
+})();
+
+
